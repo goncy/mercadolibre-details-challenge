5a911b431696276ca2e2913aed1ebf5ec3956677
Fabio Guardado <fguardado@applaudostudios.com>
2022-01-11 18:16:04 -0600

    Update navbar component position

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import {Container, Stack, Box} from "@chakra-ui/react";
 
 import Routes from "./routes";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 const App: React.FC = () => {
   return (
-    <Container alignSelf="center" maxWidth="container.xl" paddingX={0}>
-      <Stack>
-        <Box>{`< Navbar />`}</Box>
-        <Box>
-          <Routes />
-        </Box>
-      </Stack>
-    </Container>
+    <>
+      <Navbar />
+      <Container alignSelf="center" maxWidth="container.xl" paddingX={0}>
+        <Stack>
+          <Box>
+            <Routes />
+          </Box>
+        </Stack>
+      </Container>
+    </>
   );
 };
 
